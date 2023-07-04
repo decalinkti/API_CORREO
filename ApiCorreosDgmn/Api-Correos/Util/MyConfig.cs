dceba5056a8ccb72247f28403518a4fe74591b7f
pablosalinasc <pablo.salinasc@usach.cl>
2023-07-04 13:39:44 -0400

    Corrección parámetros sobrantes

--- ApiCorreosDgmn/Api-Correos/Util/MyConfig.cs
+++ ApiCorreosDgmn/Api-Correos/Util/MyConfig.cs
@@ -9,13 +9,10 @@ namespace Api_Correos.Util
     public class MyConfig
     {
         public int PuertoAPIPersistencia { get; set; }
-        public int PuertoAPIPDFServer { get; set; }
         public string URLPersistencia { get; set; }
-        public string URLPDFServer { get; set; }
         public clienteAPI ClienteApiCorreo { get; set; }
         public AES AES { get; set; }
         public JWT JWT { get; set; }
         public string TokenPersistencia { get; set; }
-        public string TokenPDFServer { get; set; }
     }
 }
